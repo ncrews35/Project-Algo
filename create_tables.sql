e26c46a57507dd57be0e30568e0d17c21bb6064d
ncrews35 <nickcrews35@gmail.com>
2021-02-01 15:10:30 -0500

    Updated Queries

--- create_tables.sql
+++ create_tables.sql
@@ -1,18 +1,37 @@
-CREATE TABLE stock {
+CREATE TABLE stock (
     id SERIAL PRIMARY KEY,
     symbol TEXT NOT NULL,
     name TEXT NOT NULL,
     exchange TEXT NOT NULL,
     is_etf BOOLEAN NOT NULL
-};
+);
 
-CREATE TABLE mention {
-    stock_id INTEGER,
+CREATE TABLE mention (
+    stock_id INTEGER NOT NULL,
     dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     message TEXT NOT NULL, 
-    source TEXT NOT NULL, -- wsb, twitter, ect.
+    source TEXT NOT NULL,
     url TEXT NOT NULL,
-    PRIMARY KEY (stock_id, dt):
-    CONSTRAINT fk_mention_stock DOREIGN KEY (stock_id) REFERENCES stock (id)
-};
+    PRIMARY KEY (stock_id, dt),
+    CONSTRAINT fk_mention_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
+);
 
+CREATE INDEX ON mention (stock_id, dt DESC);
+SELECT create_hypertable('mention', 'dt');
+
+CREATE TABLE stock_price (
+    stock_id INTEGER NOT NULL,
+    dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    open NUMERIC NOT NULL,
+    high NUMERIC NOT NULL,
+    low NUMERIC NOT NULL,
+    close NUMERIC NOT NULL,
+    volume NUMERIC NOT NULL,
+    PRIMARY KEY (stock_id, dt),
+    CONSTRAINT dk_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
+);
+
+
+
+CREATE INDEX ON stock_price (stock_id, dt DESC);
+SELECT create_hypertable('stock_price', 'dt');
